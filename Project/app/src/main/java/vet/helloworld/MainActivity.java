1853187cd9af8d38eccde569287a1bdc3fdb7291
avitaliev <avitaliev@transas.com>
2017-08-16 11:40:52 +0300

    Еще один коммит

--- Project/app/src/main/java/vet/helloworld/MainActivity.java
+++ Project/app/src/main/java/vet/helloworld/MainActivity.java
@@ -2,12 +2,21 @@ package vet.helloworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mHelloTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mHelloTextView = (TextView)findViewById(R.id.textView);
+    }
+
+    public void onClick(View view) {
+        mHelloTextView.setText("Hi");
     }
 }
